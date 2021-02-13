67127f20b420f0f8e7a3c1bd3596fc964df7206b
Nicolas Constanty <nicolas.constanty@outlook.fr>
2021-02-13 02:57:37 +0100

    Update appveyorlinux

--- build_linux.sh
+++ build_linux.sh
@@ -6,4 +6,4 @@ export CC="ccache gcc"
 rm -rf build/lib.linux-x86_64-3.7
 ThirdParties/python/bin/python3.7m setup.py build
 cp build/lib.linux-x86_64-3.7/* mesh_autoremesher/src
-zip -r -u mesh_autoremesher.zip mesh_autoremesher/
+zip -r -u mesh_autoremesher_linux.zip mesh_autoremesher/
